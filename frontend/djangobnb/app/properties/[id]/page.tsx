71e7a34b7eef77403eb83b3879763b5734358c6a
KrystianAnd <krystianandrzejak7@gmail.com>
2025-04-23 21:37:20 +0200

    Properties
    some parts of Properties page

---
+++ frontend/djangobnb/app/properties/[id]/page.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import ReservationSideBar from "@/app/components/Properties/ReservationSideBar";
+const PropertyDetailPage = () => {
+    return(
+        <main className="max-w-[1500px] mx-auto px-6 pb-6">
+            <div className="w-full h-[64vh] mt-4 overflow-hidden rounded-xl relative">
+                <Image
+                    fill
+                    src="/beach_1.jpg"
+                    className="object-cover w-full h-full"
+                    alt="Beach house"
+
+                />
+            </div>
+
+            <div className=" grid grid-cols-1 md:grid-cols-5 gap-4">
+                <div className="py-6 pr-6 col-span-3">
+                    <h1 className="mb-4 text-4xl">Property Name</h1>
+
+                    <span className="mb-6 block text-lg text-gray-600">
+                        4 guests - 2 bedrooms - 1 bathroom
+                    </span>
+
+                    <hr />
+
+                    <div className="py-6 flex items-center space-x-4 ">
+                        <Image
+                            src="/profile_pic_1.jpg"
+                            width={50}
+                            height={50}
+                            className="rounded-full"
+                            alt="The User name"
+                        />
+
+                        <p><strong>John Deo</strong> is your host</p>
+                    </div>
+
+                    <hr />
+
+                    <div className="mt-6 text-lg ">
+                        hebfbwjvfbejwbvfwjubfybfbjuwebvfjjufwvv
+                    </div>
+                </div>
+                <ReservationSideBar />
+            </div>
+        </main>
+    )
+}
+
+export default PropertyDetailPage;
