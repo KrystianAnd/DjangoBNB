bd1de238f8277be2659ec2038a1b9585662409ec
KrystianAnd <krystianandrzejak7@gmail.com>
2025-04-30 13:51:43 +0200

    api for properties

--- frontend/djangobnb/app/components/Properties/PropertyList.tsx
+++ frontend/djangobnb/app/components/Properties/PropertyList.tsx
@@ -1,10 +1,39 @@
+'use client';
+
+import { useEffect , useState } from "react";
 import PropertyListItem from "./PropertyListItem";
+import apiService from "@/app/services/apiService";
+export type PropertyType = {
+    id: string;
+    title: string;
+    image_url: string;
+    price_per_night: number;
+}
+
 const PropertyList = () => {
+    const [properties, setProperties] = useState<PropertyType[]>([]);
+    const getProperties = async () => {
+        const url = '/api/properties/';
+
+        const tmpProperties = await apiService.get(url)
+
+        setProperties(tmpProperties.data);
+    };
+
+    useEffect(() =>{
+        getProperties();
+    }, []);
+
     return(
         <>
-            <PropertyListItem />
-            <PropertyListItem />
-            <PropertyListItem />
+            {properties.map((property) =>{
+                return(
+                    <PropertyListItem 
+                        key={property.id}
+                        property={property}
+                    />
+                )
+            })}
         </>
     )
 }
