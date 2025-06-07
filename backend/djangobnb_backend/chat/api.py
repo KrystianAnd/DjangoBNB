4450cf43e224ea173d73aa2cdc578dc89e8dedc2
KrystianAnd <krystianandrzejak7@gmail.com>
2025-06-07 21:19:57 +0200

    implemented ruff
    implemented ruff and starts to clear errors

--- backend/djangobnb_backend/chat/api.py
+++ backend/djangobnb_backend/chat/api.py
@@ -1,36 +1,43 @@
-from django.http import JsonResponse
+from django.http import HttpRequest, JsonResponse
 from rest_framework.decorators import api_view
-from .models import Conversation, ConversationMessage
-from .serializers import ConversationsListSerializer, ConversationDetailSerializer, ConversationMessageSerializer
 from useraccount.models import User
 
-@api_view(['GET'])
-def conversations_list(request):
-    serializer = ConversationsListSerializer(request.user.conversations.all(), many=True)
+from .models import Conversation
+from .serializers import (
+    ConversationDetailSerializer,
+    ConversationMessageSerializer,
+    ConversationsListSerializer,
+)
 
+
+@api_view(["GET"])
+def conversations_list(request: HttpRequest) -> JsonResponse:
+    serializer = ConversationsListSerializer(request.user.conversations.all(), many=True)
     return JsonResponse(serializer.data, safe=False)
 
-@api_view(['GET'])
-def conversations_detail(request, pk):
+
+@api_view(["GET"])
+def conversations_detail(request: HttpRequest, pk: int) -> JsonResponse:
     conversation = request.user.conversations.get(pk=pk)
 
     conversation_serializer = ConversationDetailSerializer(conversation, many=False)
     messages_serializer = ConversationMessageSerializer(conversation.messages.all(), many=True)
 
-    return JsonResponse({
-        'conversation': conversation_serializer.data,
-        'messages': messages_serializer.data
-    }, safe=False)
+    return JsonResponse(
+        {"conversation": conversation_serializer.data, "messages": messages_serializer.data},
+        safe=False,
+    )
 
-@api_view(['GET'])
-def conversations_start(requset, user_id):
-    conversations = Conversation.objects.filter(users__in=[user_id]).filter(users__in=[requset.user.id])
-    if conversations.count() >0:
+
+@api_view(["GET"])
+def conversations_start(request: HttpRequest, user_id: int) -> JsonResponse:
+    conversations = Conversation.objects.filter(users__in=[user_id]).filter(users__in=[request.user.id])
+    if conversations.exists():
         conversation = conversations.first()
-        return JsonResponse({'success':True, 'conversation_id': conversation.id})
-    else:
-        user = User.objects.get(pk=user_id)
-        conversation = Conversation.objects.create()
-        conversation.users.add(requset.user)
-        conversation.users.add(user)
-        return JsonResponse({'success':True, 'conversation_id': conversation.id})
+        return JsonResponse({"success": True, "conversation_id": conversation.id})
+
+    user = User.objects.get(pk=user_id)
+    conversation = Conversation.objects.create()
+    conversation.users.add(request.user)
+    conversation.users.add(user)
+    return JsonResponse({"success": True, "conversation_id": conversation.id})
